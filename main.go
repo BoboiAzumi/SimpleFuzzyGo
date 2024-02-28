ecf7b323e396ef7c7b1cdb69b7a86cbf925d1e75
BoboiAzumi <untukherokudeploy@gmail.com>
2024-02-28 18:19:03 +0700

    New Project

--- main.go
+++ main.go
@@ -4,22 +4,21 @@ import (
 	"fmt"
 	"simplefuzzy/Fuzzy"
 	"strconv"
-	"strings"
 )
 
 func main() {
 	FL := Fuzzy.NewFuzzyLogic()
 
-	FL.SetMinDomain("Brrrr     ", 0, 10.0)
-	FL.SetMaxDomain("Panas beud", 80.0, 100.0)
+	FL.SetMinDomain("Brrrr", 0, 10.0)
+	FL.SetMaxDomain("Bekasi", 80.0, 100.0)
 
-	FL.AddTriangleFunction(5.0, 10.0, 15.0, "Dingin     ")
-	FL.AddTriangleFunction(10.0, 15.0, 20.0, "Sejuk     ")
-	FL.AddTriangleFunction(15.0, 20.0, 25.0, "Nyaman    ")
-	FL.AddTriangleFunction(20.0, 25.0, 30.0, "Biasa     ")
-	FL.AddTriangleFunction(25.0, 30.0, 35.0, "Hangat    ")
-	FL.AddTriangleFunction(30.0, 35.0, 40.0, "Gerah     ")
-	FL.AddTriangleFunction(35.0, 60.0, 90.0, "Panas     ")
+	FL.AddTriangleFunction(5.0, 10.0, 15.0, "Dingin")
+	FL.AddTriangleFunction(10.0, 15.0, 20.0, "Sejuk")
+	FL.AddTriangleFunction(15.0, 20.0, 25.0, "Nyaman")
+	FL.AddTriangleFunction(20.0, 25.0, 30.0, "Biasa")
+	FL.AddTriangleFunction(25.0, 30.0, 35.0, "Hangat")
+	FL.AddTriangleFunction(30.0, 35.0, 40.0, "Gerah")
+	FL.AddTriangleFunction(35.0, 60.0, 90.0, "Panas")
 
 	// Isi Temperature Disini
 	var temp float32 = 13.5
@@ -35,5 +34,5 @@ func main() {
 
 	MaxWeight := FL.GetMaxWeight()
 
-	fmt.Printf("\nBerdasarkan bobot diatas dapat disimpulkan bahwa %s°C adalah %s\ndengan bobot %f\n", strconv.FormatFloat(float64(temp), 'f', -1, 32), strings.TrimSpace(MaxWeight.Label), MaxWeight.Weight)
+	fmt.Printf("\nBerdasarkan bobot diatas dapat disimpulkan bahwa %s°C adalah %s\ndengan bobot %f\n", strconv.FormatFloat(float64(temp), 'f', -1, 32), MaxWeight.Label, MaxWeight.Weight)
 }
